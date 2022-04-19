2f2829b462b985ac3217a4cb4933dd961fb2e7ce
Slavik-Art <slavik.adobe@gmail.com>
2022-04-19 13:43:22 +0300

    Добавьте файлы проекта.

---
+++ Family_Tree/Family_Tree.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(console, 1);
+    cout << R"(------------------------------------------------------------)" << "\n";
+    cout << R"(|                                                          |)" << "\n";
+    SetConsoleTextAttribute(console, 14);
+    cout << R"(|                Ivanov Petya Vasiliyvich                  |)" << "\n";
+    SetConsoleTextAttribute(console, 1);
+    cout << R"(|                          |                               |)" << "\n";
+    cout << R"(|                         / \                              |)" << "\n";
+    cout << R"(|                        /   \                             |)" << "\n";
+    cout << R"(|                       /     \                            |)" << "\n";
+    cout << R"(|                      /       \                           |)" << "\n";
+    cout << R"(|                ------         ------                     |)" << "\n";
+    cout << R"(|               /                     \                    |)" << "\n";
+    cout << R"(|              /                       \                   |)" << "\n";
+    cout << R"(|             /                         \                  |)" << "\n";
+    cout << R"(|            /                           \                 |)" << "\n";
+    cout << R"(|           /                             \                |)" << "\n";
+    SetConsoleTextAttribute(console, 14);
+    cout << R"(| Ivanov Vasya Genadich          Ivanova Lena Sergeevna    |)" << "\n";
+    SetConsoleTextAttribute(console, 1);
+    cout << R"(|          |                               |               |)" << "\n";
+    cout << R"(|         / \                             / \              |)" << "\n";
+    cout << R"(|        /   \                           /   \             |)" << "\n";
+    cout << R"(|       /     \                         /     \            |)" << "\n";
+    cout << R"(|      /       \                       /       \           |)" << "\n";
+    cout << R"(|     /         \                     /         \          |)" << "\n";
+    cout << R"(|    /           \                   /           \         |)" << "\n";
+    SetConsoleTextAttribute(console, 14);
+    cout << R"(|Ded Gena   Babushka Alya       Ded Sergey   Babushka Katya|)" << "\n";
+    SetConsoleTextAttribute(console, 1);
+    cout << R"(|-----------------------------------------------------------)" << "\n";
+    SetConsoleTextAttribute(console, 7);
+}
